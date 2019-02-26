5c5d5485ff50d75b071e99b9f66ef6782f4bdd0d
Unix&Me <unixnme@gmail.com>
2019-02-26 12:03:42 +0900

    add shape descriptions

--- torchfcn/models/fcn32s.py
+++ torchfcn/models/fcn32s.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+import pdb
+
 
 # https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/surgery.py
 def get_upsampling_weight(in_channels, out_channels, kernel_size):
@@ -39,18 +41,18 @@ class FCN32s(nn.Module):
     def __init__(self, n_class=21):
         super(FCN32s, self).__init__()
         # conv1
-        self.conv1_1 = nn.Conv2d(3, 64, 3, padding=100)
+        self.conv1_1 = nn.Conv2d(3, 64, 3, padding=100) # x+200-2
         self.relu1_1 = nn.ReLU(inplace=True)
         self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
         self.relu1_2 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/2
+        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # ceil( (x+198)/2 )
 
         # conv2
         self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
         self.relu2_1 = nn.ReLU(inplace=True)
         self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
         self.relu2_2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/4
+        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # ceil( (x+198)/4 )
 
         # conv3
         self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
@@ -59,7 +61,7 @@ class FCN32s(nn.Module):
         self.relu3_2 = nn.ReLU(inplace=True)
         self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
         self.relu3_3 = nn.ReLU(inplace=True)
-        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/8
+        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # ceil( (x+198)/8 )
 
         # conv4
         self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
@@ -68,7 +70,7 @@ class FCN32s(nn.Module):
         self.relu4_2 = nn.ReLU(inplace=True)
         self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
         self.relu4_3 = nn.ReLU(inplace=True)
-        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/16
+        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # ceil( (x+198)/16 )
 
         # conv5
         self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
@@ -77,10 +79,10 @@ class FCN32s(nn.Module):
         self.relu5_2 = nn.ReLU(inplace=True)
         self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
         self.relu5_3 = nn.ReLU(inplace=True)
-        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/32
+        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # ceil( (x+198)/32 ) = ceil( (x+6)/32 ) + 6
 
         # fc6
-        self.fc6 = nn.Conv2d(512, 4096, 7)
+        self.fc6 = nn.Conv2d(512, 4096, 7) # ceil( (x+6)/32 )
         self.relu6 = nn.ReLU(inplace=True)
         self.drop6 = nn.Dropout2d()
 
@@ -89,9 +91,10 @@ class FCN32s(nn.Module):
         self.relu7 = nn.ReLU(inplace=True)
         self.drop7 = nn.Dropout2d()
 
-        self.score_fr = nn.Conv2d(4096, n_class, 1)
+        self.score_fr = nn.Conv2d(4096, n_class, 1) # ceil( (x+6)/32 )
         self.upscore = nn.ConvTranspose2d(n_class, n_class, 64, stride=32,
                                           bias=False)
+        # ceil( (x+6)/32 )*32 + 32
 
         self._initialize_weights()
 
@@ -109,6 +112,7 @@ class FCN32s(nn.Module):
 
     def forward(self, x):
         h = x
+        pdb.set_trace()
         h = self.relu1_1(self.conv1_1(h))
         h = self.relu1_2(self.conv1_2(h))
         h = self.pool1(h)
